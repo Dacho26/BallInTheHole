c4689cc635c36d6a72fff3a2869f9ca5940efc8b
Dacho26 <nastevskidavid6@gmail.com>
2019-02-17 10:03:34 +0100

    .
    .

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let ball = document.querySelector(".ball");
 let area = document.querySelector("#area");
+let panel = document.querySelector("p a");
+let active = false;
+let time = 0;
+let idI;
 
 let maxX = area.clientWidth - ball.clientWidth;
 let maxY = area.clientHeight - ball.clientHeight;
@@ -19,6 +23,28 @@ const handleOrientation = () => {
 
     ball.style.top = (maxX * beta / 180 - 15) + "px";
     ball.style.left = (maxY * gamma / 180 - 15) + "px";
+
+}
+const timer = () => {
+    if (!active) {
+        active = !active;
+        idI = setInterval(start, 10);
+    } else {
+        active = !active;
+        clearInterval(idI);
+    }
 }
 
+const start = () => {
+    time++;
+    panel.textContent = (time / 100).toFixed(2);
+}
+const stop = () => {
+    time = 0;
+    active = false;
+
+    clearInterval(idI);
+}
+
+window.addEventListener("deviceorientation", timer)
 window.addEventListener("deviceorientation", handleOrientation)
